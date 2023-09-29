afb590892f7af7c09aa1df9abcd42bff4c53ae28
Maulana Arif Wijaya <maulanatoyek@gmail.com>
2023-09-29 15:26:30 +0700

    feat: finish week 5

---
+++ week-05/src/hello_world/lib/basic_widgets/fab_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyFab extends StatelessWidget {
+  const MyFab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // Add your onPressed code here!
+          },
+          backgroundColor: Colors.pink,
+          child: const Icon(Icons.thumb_up),
+        ),
+      ),
+    );
+  }
+}
